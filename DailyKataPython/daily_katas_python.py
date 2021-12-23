16f44c176a2ec0f62cd7ceb066052bf33b099ef4
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2021-12-23 15:43:04 +0100

    python1

--- DailyKataPython/daily_katas_python.py
+++ DailyKataPython/daily_katas_python.py
@@ -8,3 +8,20 @@ def duplicate_count(text):
             duplicates += 1
         text = text.replace(case, "") 
     return duplicates
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+def median(array):
+    array.sort()
+    array_len = len(array)
+    if array_len % 2:
+        index = int(array_len / 2)
+        return array[index]
+    else:
+        index = int(array_len / 2)
+        index2 = int(array_len / 2) - 1
+        return (array[index] + array[index2] / 2.0)
--- DailyKataPython/test_katas_python.py
+++ DailyKataPython/test_katas_python.py
@@ -1,5 +1,7 @@
 import unittest
 from daily_katas_python import duplicate_count
+from daily_katas_python import even_or_odd
+from daily_katas_python import median
 
 class Duplicate_count_1(unittest.TestCase):
     def test_A(self):
@@ -20,6 +22,42 @@ class Duplicate_count_4(unittest.TestCase):
 class Duplicate_count_5(unittest.TestCase):
     def test_A(self):
         self.assertEqual(duplicate_count("Indivisibilities"), 2)
+
+class Even_or_odd_1(unittest.TestCase):
+    def test_A(self):
+        self.assertEqual(even_or_odd(2), "Even")
+    def test_B(self):
+        self.assertEqual(even_or_odd(1), "Odd")
+    def test_C(self):
+        self.assertEqual(even_or_odd(0), "Even")
+    def test_D(self):
+        self.assertEqual(even_or_odd(1545452), "Even")
+    def test_E(self):
+        self.assertEqual(even_or_odd(7), "Odd")
+    def test_F(self):
+        self.assertEqual(even_or_odd(78), "Even")
+    def test_G(self):
+        self.assertEqual(even_or_odd(17), "Odd")
+    def test_H(self):
+        self.assertEqual(even_or_odd(74156741), "Odd")
+    def test_I(self):
+        self.assertEqual(even_or_odd(100000), "Even")
+    def test_J(self):
+        self.assertEqual(even_or_odd(-123), "Odd")
+    def test_K(self):
+        self.assertEqual(even_or_odd(-456), "Even")
+
+class Find_median(unittest.TestCase):
+    def test_A(self):
+        self.assertEqual(median([3,2,1]),2)
+    def test_B(self):
+        self.assertEqual(median([1]),1)
+    def test_C(self):
+        self.assertEqual(median([1234,345,78]),345)
+    def test_D(self):
+        self.assertEqual(median([33,99,100,30,29,50]), 41.5)
+    def test_E(self):
+        self.assertEqual(median([3,50]), 26.5)
 
 if __name__ == '__main__':
     unittest.main()
